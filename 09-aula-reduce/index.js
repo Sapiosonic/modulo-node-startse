9a60d458cd24bdbbe396165c2ef1d691df4604f6
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-12-12 00:52:01 -0300

    working with reduce function

---
+++ 09-aula-reduce/index.js
@@ -0,0 +1,13 @@
+import { getPeople } from "./StarWarsService.js";
+
+async function main() {
+  const starWarsPeople = await getPeople(1);
+  const totalHeight = starWarsPeople.reduce((total, person) => {
+    total += Number(person.height);
+    return total;
+  }, 0)
+
+  console.log("A média da altura: ", totalHeight / Number(starWarsPeople.length));
+}
+
+main();
