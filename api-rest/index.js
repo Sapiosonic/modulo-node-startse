956ead3d4f526bf98da3a2ee0e834c9c0fd1fe16
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-12-12 01:14:21 -0300

    Working with GET

--- api-rest/index.js
+++ api-rest/index.js
@@ -2,6 +2,18 @@ import express from 'express';
 
 const app = express();
 const PORT = 3000;
+let users = [
+  {
+    id: 1,
+    name: "Radamés",
+    age: 34,
+  },
+  {
+    id: 2,
+    name: "Camila",
+    age: 29,
+  },
+];
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
@@ -9,4 +21,18 @@ app.listen(PORT, () => {
 
 app.get('/', (request, response) => {
   return response.send('<h1>Trabalhando com servidor express.</h1>');
+});
+
+//this route returns a list of users
+app.get('/users', (request, response) => {
+  return response.send(users);
+});
+
+//this route returns a specific user in a list of users using method find
+app.get('/users/:userId', (request, response) => {
+  const userId = request.params.userId;
+  const user = users.find(user => {
+    return (user.id === Number(userId))
+  })
+  return response.send(user)
 })
