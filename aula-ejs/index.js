15b86a24201383631f4a606a41b4b382ba7c3f80
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-12-14 11:10:25 -0300

    Working with EJS variables

--- aula-ejs/index.js
+++ aula-ejs/index.js
@@ -4,12 +4,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-app.set('views', './views')
+app.set('views', './views');
 
 app.listen(PORT, ()=> {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 
 app.get('/', (request, response) => {
-  response.render('pages/home', {});
+  response.render('pages/home', {title: 'EJS'});
 });
